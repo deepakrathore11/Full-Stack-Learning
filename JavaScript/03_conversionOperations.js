121dc68fdfee192a276b06b6cf4686f61fc26af9
Deepak Rathore <deepakrathor485@gmail.com>
2026-05-15 17:04:44 +0530

    conversion of datatypes

---
+++ JavaScript/03_conversionOperations.js
@@ -0,0 +1,51 @@
+let score = "33abc"
+
+console.log(score);
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// "abc" => NaN
+// true => 1  
+// false => 0
+// null => 0
+// undefined => NaN
+
+
+let isLoggedIn = 0
+ let booleamIsLoggedIn = Boolean(isLoggedIn);
+ console.log(booleamIsLoggedIn);
+
+ // 1 => true; 0 => false
+ // "" => false
+ // "Deepak" => true
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber);
+
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
