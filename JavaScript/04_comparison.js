5f99e14d2c5ebef1b244f2782e220387a6f49e4b
Deepak Rathore <deepakrathor485@gmail.com>
2026-05-18 01:28:30 +0530

    Comparison

---
+++ JavaScript/04_comparison.js
@@ -0,0 +1,32 @@
+// This is Good comparison
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <=  1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// This is Bad comparison
+console.log("2" > 1);
+console.log(1 < "2");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null != 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined != 0);
+
+// === strict equality operator 
+console.log("2" === 2);
+
+
+
+
+
+
