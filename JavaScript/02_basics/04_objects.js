cf81ed99a8ba3b644304ad7709e85ad6a2619967
Deepak Rathore <deepakrathor485@gmail.com>
2026-05-24 01:26:10 +0530

    object

--- JavaScript/02_basics/04_objects.js
+++ JavaScript/02_basics/04_objects.js
@@ -1,11 +1,9 @@
 // const tinderUser = new Object() // singleton object
-
-const tinderUser = {} // non singleton object
+const tinderUser = {} // non-singleton object
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
-// console.log(tinderUser);
-
+console.log(tinderUser);
 
 const regularUser = {
     email: "deepak@gmail.com",
@@ -16,17 +14,43 @@ const regularUser = {
         }
     }
 }
-
 console.log(regularUser.fullname.userFullName.firstname); // this is how you can access the object that is nested in object.
 
-
+// object concat
 const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-
-// Object.assign(target{}, source= obj1, obj2);
-
-// const obj3 = Object.assign({}, obj1, obj2); // this is use for concat the objects. 
-
-const obj3 = {...obj1, ...obj2} // this is the best way.
-
+const obj2 = {3: "a", 4: "b"} 
+// Object.assign(target{}, source= obj1, obj2); 
+const obj3 = Object.assign({}, obj1, obj2);
+// const obj3 = {...obj1, ...obj2} // but this is the best way.
 console.log(obj3);
+
+// Array of Objects
+const users =[
+    {
+        id: 1,
+        email: "deepakrathor1@google.com"
+
+    },
+    {
+        id: 2,
+        email: "deepakrathor2@google.com"
+
+    },
+    {
+        id: 3,
+        email: "deepakrathor3@google.com"
+
+    }
+
+
+]
+
+console.log(users[0].id);
+console.log(users[0].email);
+
+// Object methods imp & useful.
+console.log(tinderUser);
+console.log(Object.keys(tinderUser),`These are the keys`);
+console.log(Object.values(tinderUser), `These are the values`);
+console.log(Object.entries(tinderUser), `These are the entries`);
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // True because it has.
